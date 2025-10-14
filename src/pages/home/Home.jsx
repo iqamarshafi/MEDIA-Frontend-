e6dfcccaa5e9d839f5c00db3cf70768747408b35
Shafi Qamar <qamarshafi124@gmail.com>
2025-10-14 14:45:19 +0530

    fix import path for navbar in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { axiosClient } from '../../utils/axiosClient';
-import Navbar from '../../components/navbar/navbar';
+import Navbar from '../../components/navbar/Navbar.jsx';
 import { Outlet } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getMyInfo } from '../../redux/slices/appConfigSlice';
